4a9dfc6ba319b1337d75645e11c90546571c9af3
flavius <flavius.popoiu@mediaimage.ro>
2016-11-07 16:42:40 +0200

    regexps 1

--- chapter1/less1.py
+++ chapter1/less1.py
@@ -1,1 +1,30 @@
-#noul corepy
+import re
+
+# bri = 'tzuika'
+# pattern = '[w]{4}'
+# m = re.findall(pattern, bri)
+# for i in m:
+#     print(i)
+# m = re.compile(pattern)
+# if m is not None:
+#     hm = m.sub("whisk", bri)
+#     print(hm)
+
+# theStr = "F.B.I N.S.A C.IA"
+# thePattern = '.\..\..'
+# m = re.findall(thePattern, theStr)
+# for i in m:
+#     print(i)
+
+# theStr = '''This is
+# a long string
+# that goes for many lines
+# '''
+#print(theStr)
+
+# regex = re.compile("\n")
+# theStr = regex.sub(" ", theStr)
+# print(theStr)
+
+# randStr = '12345'
+# print("Number of matches {}".format(len(re.findall("\d{1}", randStr))))
