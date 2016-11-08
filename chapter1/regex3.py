8c871dc7357754728b90307aabc220226c4ea970
flavius <flavius.popoiu@mediaimage.ro>
2016-11-08 16:55:15 +0200

    regexps 3

--- chapter1/regex3.py
+++ chapter1/regex3.py
@@ -19,5 +19,39 @@ import re
 # regex = re.compile(r"([\d]{3})-([\d]{3}-[\d]{4})")
 # randStr = re.sub(regex, r"(\1)\2", randStr)
 # print(randStr)
-randStr = "https://www.youtube.com https://www.google.com"
-regex = re.compile()
+# randStr = "https://www.youtube.com http://www.google.com"
+# regex = re.compile(r"(https?://([\w.]+))")
+# randStr = re.sub(regex, r"<a href='\1'>\2</a>\n", randStr)
+# print(randStr)
+
+# lookahead is a pattern to match, but not return
+# (?=expression)
+
+#randStr = "One two three four"
+#we want the letters and numbers, but not the spaces
+# regex = re.compile(r"\w+(?=\b)")
+# matches = re.findall(regex, randStr)
+# for i in matches:
+#     print(i)
+
+#the look behind (?<=expression)
+# randStr = "1. Bread 2. Apples 3. Lettuce"
+# regex = re.compile(r"(?<=\d.\s)\w+")
+# matches = re.findall(regex, randStr)
+# for i in matches:
+#     print(i)
+
+# randStr = "<h1>I am important</h1> <h1>So am I</h1>"
+# regex = re.compile(r"(?<=<h1>).+?(?=</h1>)")
+# matches = re.findall(regex, randStr)
+# for i in matches:
+#     print(i)
+#Negative lookahead/lookbehind (text that does not match pattern)
+#Negative lookahead (?!expression)
+#Negative lookbehind (?<!expression)
+randStr = "8 Apples $3, 1 Bread $1, 1 Cereal $4"
+regex = re.compile(r"(?<!\$)\d+")
+matches = re.findall(regex, randStr)
+
+for i in matches:
+    print(i)
