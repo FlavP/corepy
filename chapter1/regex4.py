376aa78411834338264e40910b453774ecd06470
flavius <flavius.popoiu@mediaimage.ro>
2016-11-10 14:16:01 +0200

    regexps 4

--- chapter1/regex4.py
+++ chapter1/regex4.py
@@ -8,8 +8,23 @@ import re
 # for i in matches:
 #     print(i)
 
-randStr = "12345 12345-1234 1234 13543-222"
-regex = re.compile(r"\d{5}\s|\d{5}\-\d{4}\s")
-matches = re.findall(regex, randStr)
-for i in matches:
-    print(i)
+# randStr = "12345 12345-1234 1234 13543-222"
+# regex = re.compile(r"\d{5}\s|\d{5}\-\d{4}\s")
+# matches = re.findall(regex, randStr)
+# for i in matches:
+#     print(i)
+#group
+# bd = input("Enter your birthday (mm-dd-yyyy) : ")
+#
+# bdRegex = re.search(r"(\d{1,2})-(\d{1,2})-(\d{4})", bd)
+# print("You were born on", bdRegex.group())
+# print("Your birth Month", bdRegex.group(1))
+# print("Your birth Year", bdRegex.group(2))
+# print("Your birth Day", bdRegex.group(3))
+#named group
+randStr = "December 21 1974"
+regex = r"^(?P<month>\w+)\s(?P<day>\d+)\s(?P<year>\d{4})"
+matches = re.search(regex, randStr)
+print("Month :", matches.group('month'))
+print("Day :", matches.group('day'))
+print("Year :", matches.group('year'))
